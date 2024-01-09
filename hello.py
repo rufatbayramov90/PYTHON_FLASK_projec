69cfb33d2021ff6dbcebdfdadf23136fb8288999
rufatbayramov90 <78435344+rufatbayramov90@users.noreply.github.com>
2024-01-10 00:01:54 +0400

    HTML Elements with Flask

--- hello.py
+++ hello.py
@@ -6,7 +6,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return ' Hello, World'
+    return '<h1 style="text-align: center">Hello, World</h1>'\
+            '<p>This is a paragraph </p>'\
+            'img src="<div style="width:480px"><iframe allow="fullscreen" frameBorder="0" height="853" src="https://giphy.com/embed/paWQwjlM2mKKGa2vsR/video" width="480"></iframe></div>"'
 
 @app.route('/bye')
 def say_bye():
